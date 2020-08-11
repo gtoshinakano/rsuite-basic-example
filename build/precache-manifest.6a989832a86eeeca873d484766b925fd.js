f91d2bcb4fcfdabbfec2720c27649f7cd79645d0
Toshi <gtoshinakano@gmail.com>
2020-08-11 15:42:38 +0900

    Removed /build from .gitignore for deployment on ESP32

---
+++ build/precache-manifest.6a989832a86eeeca873d484766b925fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eed7e72c41b1f55c60c316b3f008dd06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac0fd4f0d3d05422f36d",
+    "url": "/static/css/2.fd3fe366.chunk.css"
+  },
+  {
+    "revision": "2b4ec71f4d4245d94fb1",
+    "url": "/static/css/main.e9ca466b.chunk.css"
+  },
+  {
+    "revision": "ac0fd4f0d3d05422f36d",
+    "url": "/static/js/2.e34fb58d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.e34fb58d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b4ec71f4d4245d94fb1",
+    "url": "/static/js/main.5cdc0689.chunk.js"
+  },
+  {
+    "revision": "8965a1f94d2092bf6896",
+    "url": "/static/js/runtime-main.a7fb8553.js"
+  }
+]);
